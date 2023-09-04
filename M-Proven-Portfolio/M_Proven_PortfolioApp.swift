094c18c9647e6e9afea4eed7ea8a7596496bb529
Mike-Proven <m.proven.it@gmail.com>
2023-09-04 08:51:55 -0700

    init the commits, started the git with .gitignore this time

---
+++ M-Proven-Portfolio/M_Proven_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  M_Proven_PortfolioApp.swift
+//  M-Proven-Portfolio
+//
+//  Created by user01 on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct M_Proven_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
